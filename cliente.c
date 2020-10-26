f62d3e3f2656a3c6db92fb6ef1c44a8f3e27a907
Naomi Takemoto <naomitkm@amazon.com>
2020-10-26 00:43:56 -0300

    Automatic port choice

--- cliente.c
+++ cliente.c
@@ -19,15 +19,19 @@ int main(int argc, char **argv)
    char error[MAXLINE + 1];
    struct sockaddr_in servaddr;
 
-   if (argc != 2)
+   // Input validation
+   if (argc != 3)
    {
       strcpy(error, "uso: ");
       strcat(error, argv[0]);
       strcat(error, " <IPaddress>");
+      strcat(error, " <#Porta>");
       perror(error);
       exit(1);
    }
 
+   // Create a socket that will use internet IPV4 and will exchange information using the
+   // reliable SOCK_STREAM (TCP)
    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
    {
       perror("socket error");
@@ -36,7 +40,7 @@ int main(int argc, char **argv)
 
    bzero(&servaddr, sizeof(servaddr));
    servaddr.sin_family = AF_INET;
-   servaddr.sin_port = htons(13);
+   servaddr.sin_port = htons(atoi(argv[2])); // htons convert multi-byte integer types from host byte order to network byte order
    if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0)
    {
       perror("inet_pton error");
--- servidor.c
+++ servidor.c
@@ -13,6 +13,44 @@
 #define LISTENQ 10
 #define MAXDATASIZE 100
 
+void handle_bind_error(int error_code)
+{
+   switch (error_code)
+   {
+   case EACCES:
+      printf("The requested address is protected, and the current user has inadequate permission to access it.");
+      break;
+   case EADDRINUSE:
+      printf("The specified address is already in use");
+   case EADDRNOTAVAIL:
+      printf("The specified address is not available from the local machine.");
+   default:
+      printf("Unspecified error on bind");
+      break;
+   }
+}
+
+void handle_accept_error(int error_code)
+{
+   switch (error_code)
+   {
+   case EBADF:
+      printf("socket is not a valid file descriptor.\n");
+      break;
+   case ECONNABORTED:
+      printf("The connection to socket has been aborted.\n");
+   case EFAULT:
+      printf("The address parameter is not in a writable part of the user address space.\n");
+   case EINTR:
+      printf("The accept() system call was terminated by a signal.\n");
+   case EINVAL:
+      printf("socket is unwilling to accept connections.\n");
+   default:
+      printf("Error accepting the connection.\n");
+      break;
+   }
+}
+
 int main(int argc, char **argv)
 {
    int listenfd;
@@ -21,6 +59,7 @@ int main(int argc, char **argv)
    char buf[MAXDATASIZE];
    time_t ticks;
 
+   // Create a socket IPV4 using TCP protocol
    if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
    {
       perror("socket");
@@ -30,12 +69,21 @@ int main(int argc, char **argv)
    bzero(&servaddr, sizeof(servaddr));
    servaddr.sin_family = AF_INET;
    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-   servaddr.sin_port = htons(1024);
+   servaddr.sin_port = 0;
 
    if (bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) == -1)
    {
       perror("bind");
+      handle_bind_error(errno);
       exit(1);
+   }
+
+   struct sockaddr_in curr_addr;
+   unsigned long addr_lenght = sizeof(servaddr);
+   int status = getsockname(listenfd, (struct sockaddr *)&curr_addr, (socklen_t *)&addr_lenght);
+   if (!status)
+   {
+      printf("Port: %d\n", curr_addr.sin_port);
    }
 
    if (listen(listenfd, LISTENQ) == -1)
@@ -49,10 +97,12 @@ int main(int argc, char **argv)
       if ((connfd = accept(listenfd, (struct sockaddr *)NULL, NULL)) == -1)
       {
          perror("accept");
+         handle_accept_error(errno);
          exit(1);
       }
 
       ticks = time(NULL);
+
       snprintf(buf, sizeof(buf), "%.24s\r\n", ctime(&ticks));
       write(connfd, buf, strlen(buf));
 
